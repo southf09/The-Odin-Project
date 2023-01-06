6a801699cd7163c4ec4c935c78468500905863fc
southf09 <frankstonesouth@gmail.com>
2023-01-06 09:09:02 -0500

    Function outlines and comments explaining the code added

---
+++ bookshelf/app.js
@@ -0,0 +1,13 @@
+//The following code creates books using a constructor. The books
+//are added to bookshelf (array), which through DOM manipulation is
+//visible to the user.
+
+let bookshelf = [];
+
+function book() {
+  //constructor
+}
+
+function addBookToLibrary() {
+  //adds the book to the bookshelf
+}
