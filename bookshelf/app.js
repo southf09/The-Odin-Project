3f0b0d5192deef48ffb9f46e162f2d574ae3e591
southf09 <frankstonesouth@gmail.com>
2023-01-06 10:47:03 -0500

    Made logic that creates and appends the book details to a newly created card

--- bookshelf/app.js
+++ bookshelf/app.js
@@ -2,12 +2,60 @@
 //are added to bookshelf (array), which through DOM manipulation is
 //visible to the user.
 
-let bookshelf = [];
+const shelf = document.querySelector(".shelf");
 
-function book() {
-  //constructor
+let bookshelf = [
+  {
+    title: "The Hobbit",
+    author: "J.R.R. Tolkien",
+    pages: 375,
+    haveRead: "yes",
+  },
+];
+
+function newBook(title, author, pages, haveRead) {
+  (this.title = title),
+    (this.author = author),
+    (this.pages = pages),
+    (this.haveRead = haveRead);
 }
 
 function addBookToLibrary() {
-  //adds the book to the bookshelf
+  createCard();
+}
+
+addBookToLibrary();
+
+function createCard() {
+  //Book Cover
+  const cover = document.createElement("div");
+  cover.classList.add("book-card");
+
+  //Unordered list that will hold the book details
+  const information = document.createElement("ul");
+  information.classList.add("book-details");
+
+  //Book title
+  const bookTitle = document.createElement("li");
+  bookTitle.innerHTML = `<h3>${bookshelf[0].title}</h3>`;
+
+  //Book author
+  const bookAuthor = document.createElement("li");
+  bookAuthor.innerText = bookshelf[0].author;
+
+  //Book pages
+  const bookPages = document.createElement("li");
+  bookPages.innerText = bookshelf[0].pages;
+
+  //Have read button
+  const haveRead = document.createElement("div");
+  haveRead.classList.add("have-read-btn");
+  haveRead.innerText = "read";
+
+  shelf.appendChild(cover);
+  cover.appendChild(information);
+  information.appendChild(bookTitle);
+  information.appendChild(bookAuthor);
+  information.appendChild(bookPages);
+  cover.appendChild(haveRead);
 }
