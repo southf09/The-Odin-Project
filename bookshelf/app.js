b035fecdb86575f8ca7e1c39d1412877a9cd28b2
southf09 <frankstonesouth@gmail.com>
2023-01-09 13:32:48 -0500

    Revised JS logic. Now successfully creates books, appends them to the shelf, and removes books when desired

--- bookshelf/app.js
+++ bookshelf/app.js
@@ -1,35 +1,24 @@
-//The following code creates books using a constructor. The books
-//are added to bookshelf (array), which through DOM manipulation is
-//visible to the user.
+let bookshelf = [];
 
-const shelf = document.querySelector(".shelf");
-
-let bookshelf = [
-  {
-    title: "The Hobbit",
-    author: "J.R.R. Tolkien",
-    pages: 375,
-    haveRead: "yes",
-  },
-];
-
-function newBook(title, author, pages, haveRead) {
+function Book(title, author, pages, read) {
   (this.title = title),
     (this.author = author),
     (this.pages = pages),
-    (this.haveRead = haveRead);
+    (this.read = read);
 }
 
-function addBookToLibrary() {
-  createCard();
-}
+const shelf = document.querySelector(".shelf");
+const theHobbit = new Book("The Hobbit", "Tolkien", 540, "Yes");
+const wayKings = new Book("Way of Kings", "Sanderson", 850, "No");
 
-addBookToLibrary();
+bookshelf.push(theHobbit);
+bookshelf.push(wayKings);
 
-function createCard() {
+function createCard(num) {
   //Book Cover
   const cover = document.createElement("div");
   cover.classList.add("book-card");
+  cover.classList.add(`book${num}`);
 
   //Unordered list that will hold the book details
   const information = document.createElement("ul");
@@ -37,25 +26,71 @@ function createCard() {
 
   //Book title
   const bookTitle = document.createElement("li");
-  bookTitle.innerHTML = `<h3>${bookshelf[0].title}</h3>`;
+  bookTitle.innerHTML = `<h3>${bookshelf[num].title}</h3>`;
 
   //Book author
   const bookAuthor = document.createElement("li");
-  bookAuthor.innerText = bookshelf[0].author;
+  bookAuthor.innerText = bookshelf[num].author;
 
   //Book pages
   const bookPages = document.createElement("li");
-  bookPages.innerText = bookshelf[0].pages;
+  bookPages.innerText = bookshelf[num].pages;
 
   //Have read button
-  const haveRead = document.createElement("div");
-  haveRead.classList.add("have-read-btn");
-  haveRead.innerText = "read";
+  if (bookshelf[num].read.toLowerCase() === "yes") {
+    const haveRead = document.createElement("div");
+    haveRead.classList.add("have-read-btn");
+    haveRead.innerText = "read";
+    information.appendChild(haveRead);
+  } else {
+    const haveRead = document.createElement("div");
+    haveRead.classList.add("not-read-btn");
+    haveRead.innerText = "not read";
+    information.appendChild(haveRead);
+  }
+
+  const clearBtn = document.createElement("div");
+  clearBtn.classList.add("clear-btn");
+  clearBtn.innerText = "Clear";
 
   shelf.appendChild(cover);
   cover.appendChild(information);
+  cover.appendChild(clearBtn);
   information.appendChild(bookTitle);
   information.appendChild(bookAuthor);
   information.appendChild(bookPages);
-  cover.appendChild(haveRead);
+}
+
+const bookForm = document.querySelector(".book-form");
+const showForm = document.querySelector(".button");
+
+showForm.addEventListener("click", () => {
+  bookForm.classList.toggle("hidden");
+});
+
+const submitForm = document.querySelector(".submit-btn");
+
+submitForm.addEventListener("click", (e) => {
+  e.preventDefault();
+  let bookTitle = document.getElementById("book-title").value;
+  let bookAuthor = document.getElementById("book-author").value;
+  let bookPages = document.getElementById("book-pages").value;
+  let bookRead = document.getElementById("book-read").value;
+  const addBook = new Book(bookTitle, bookAuthor, bookPages, bookRead);
+  bookshelf.push(addBook);
+  createCard(bookshelf.length - 1);
+  bookForm.classList.toggle("hidden");
+});
+
+for (let i = 0; i < bookshelf.length; i++) {
+  createCard(i);
+}
+
+const clearBook = document.querySelectorAll(".clear-btn");
+
+for (clear of clearBook) {
+  clear.addEventListener("click", (e) => {
+    const parent = e.target.parentNode;
+    parent.remove();
+  });
 }
